9183552039fc7edbeafed8579b5dbe0ae1b316bf
Kyle Gildea <kylegildea@gmail.com>
2018-03-26 21:02:00 -0400

    Add project files.

---
+++ AlphabetCipher/Cipher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace AlphabetCipher
+{
+    class Cipher
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i+=2)
+            {
+                //encode(new StringBuilder(args[i].ToLower()),new StringBuilder(args[i+1].ToLower()));
+                decode(new StringBuilder(args[i].ToLower()), new StringBuilder(args[i + 1].ToLower()));
+            }
+        }
+        static void encode(StringBuilder keyword, StringBuilder message)
+        {
+            StringBuilder encodedMessage = new StringBuilder();
+            int i = 0;
+            for (i = 0; i < message.Length; i++)
+            {
+                encodedMessage.Append(message[i] - 97 + keyword[i % keyword.Length] > 122 ? (char)(message[i] - 97 + 
+                    keyword[i % keyword.Length] - 26) : (char)(message[i] - 97 + keyword[i % keyword.Length]));
+
+            }
+            Console.WriteLine(encodedMessage.ToString());
+        }
+
+        static void decode(StringBuilder keyword, StringBuilder message)
+        {
+            StringBuilder actualMessage = new StringBuilder();
+            int i = 0;
+            for (i = 0; i < message.Length; i++)
+            {
+                actualMessage.Append(message[i] + 97 - keyword[i % keyword.Length] < 97 ? (char)(message[i] + 97 + 26 - keyword[i % keyword.Length]) : (char)(message[i] + 97 - keyword[i % keyword.Length]));
+            }
+            Console.WriteLine(actualMessage.ToString());
+        }
+    }
+}
